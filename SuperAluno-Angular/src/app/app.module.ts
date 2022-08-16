a5921a8843bbd135db17ba16666f060d5143c7ef
Lucas Labre <lucaslabre@gmail.com>
2022-08-15 21:39:57 -0300

    Inclusão dos modules

--- SuperAluno-Angular/src/app/app.module.ts
+++ SuperAluno-Angular/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { PagesModule } from './features/pages/pages.module';
+import { ComponentsModule } from './features/components/components.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
+    PagesModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
