642f3662a21813e3867e45e4170a568396353709
Lucas Labre <lucaslabre@gmail.com>
2022-08-21 10:16:52 -0300

    Correção do nome dos arquivos da página de login

--- SuperAluno-Angular/src/app/app-routing.module.ts
+++ SuperAluno-Angular/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './features/pages/login/login.component';
+import { LoginComponent } from './features/pages/login-page/login-page.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
--- SuperAluno-Angular/src/app/features/pages/login-page/login-page.component.ts
+++ SuperAluno-Angular/src/app/features/pages/login-page/login-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
 })
 export class LoginComponent implements OnInit {
 
