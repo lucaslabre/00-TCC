86cc394b0cfe3aced45dd462c3b4d02292cfda70
Lucas Labre <lucaslabre@gmail.com>
2022-08-21 10:25:07 -0300

    Criação da home page component

--- SuperAluno-Angular/src/app/app-routing.module.ts
+++ SuperAluno-Angular/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/pages/login-page/login-page.component';
+import { HomePageComponent } from './features/pages/home-page/home-page.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomePageComponent }
 ];
 
 @NgModule({
