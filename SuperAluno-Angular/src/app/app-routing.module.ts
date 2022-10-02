b8735ab8fb57dae3ac96724a1b2a8b320ab8d35d
Lucas Labre <lucaslabre@gmail.com>
2022-10-02 17:31:18 -0300

    Correção da configuração do menu lateral

--- SuperAluno-Angular/src/app/app-routing.module.ts
+++ SuperAluno-Angular/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './features/pages/login-page/login-page.component';
 import { HomePageComponent } from './features/pages/home-page/home-page.component';
+import { AppMainComponent } from './app.main.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginPageComponent },
-  { path: 'home', component: HomePageComponent }
+  {
+    path: 'home', component: AppMainComponent, children: [
+      { path: '', component: HomePageComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- SuperAluno-Angular/src/app/app.module.ts
+++ SuperAluno-Angular/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './features/pages/pages.module';
 import { ComponentsModule } from './features/components/components.module';
+import { AppMainComponent } from './app.main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppMainComponent
   ],
   imports: [
     BrowserModule,
