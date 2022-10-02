680b22e9fbf46449e9f7e1693d57e85f58ec1bff
Lucas Labre <lucaslabre@gmail.com>
2022-10-02 20:25:44 -0300

    Teste de implementação de pastas

--- SuperAluno-Angular/src/app/shared/shared.module.ts
+++ SuperAluno-Angular/src/app/shared/shared.module.ts
@@ -9,7 +9,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 import { TopbarComponent } from './components/topbar/topbar.component';
 
@@ -21,7 +22,8 @@ const materialModules = [
   MatInputModule,
   MatIconModule,
   MatToolbarModule,
-  MatSidenavModule
+  MatSidenavModule,
+  MatListModule
 ]
 
 const angularModules = [
