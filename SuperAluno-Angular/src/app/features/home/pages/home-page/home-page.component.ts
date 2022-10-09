eb3e949311d1004382238234676fa3ef28362f20
Lucas Labre <lucaslabre@gmail.com>
2022-10-09 11:14:57 -0300

    Limpeza de variáveis desnecessárias na home-page

--- SuperAluno-Angular/src/app/features/home/pages/home-page/home-page.component.ts
+++ SuperAluno-Angular/src/app/features/home/pages/home-page/home-page.component.ts
@@ -1,12 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Materia } from '../../../../shared/models/materia.model';
-import { Caderno } from '../../../../shared/models/caderno.model';
-import { CadernoService } from '../../../cadernos/services/caderno.service';
-
-export interface Section {
-  name: string;
-  updated: Date;
-}
 
 @Component({
   templateUrl: './home-page.component.html',
@@ -15,28 +7,9 @@ export interface Section {
 
 export class HomePageComponent implements OnInit {
 
-  materias!: Materia[];
-  cadernos!: Caderno[];
-
-  constructor(
-    private cadernoService: CadernoService
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
-
-      this.materias = [
-        {
-          idCaderno : 1,
-          conteudo : "de constante<\/strong> e em linha reta.<\/p>"
-        },
-        {
-          idCaderno : 2,
-          conteudo : "teste"
-        }
-      ]
-
-      this.cadernoService.findAllCadernoByUsuario(1).subscribe((cadernos) => this.cadernos = cadernos);
-
   }
 
 }
